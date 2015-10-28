c0fd2d26190fd7b859662319e8a81a23b7f81f84
Sam Gordon <seg@well.com>
2015-10-27 21:26:59 -0700

    added percentage of total weight

--- wheelchair_ramp_calculator.py
+++ wheelchair_ramp_calculator.py
@@ -8,15 +8,19 @@ __email__ = "seg@well.com"
 import math
 
 rr = 1.005
-ramp_incline_degrees = 5.3 # ADA incline of 1/12 is 4.78deg
+ramp_incline_degrees = 4.78 # ADA incline of 1/12 is 4.78deg
 
 
 radians = math.radians(ramp_incline_degrees)
 
 # collecting data for patients up to 300 pounds,
+
+def percentage(part, whole):
+    return 100 * float(part)/float(whole)
+
 wt = 60
 for i in range(60,300):
     wt += 1
     wt_w_rr = wt * rr
     total_force = wt_w_rr * math.sin(radians)
-    print(str(wt) + " lbs   " + str("%.2f" % total_force) + " " + "lbs " + "  " + str(ramp_incline_degrees) + " degree incline" )
+    print(str(wt) + " lbs   " + str("%.2f" % total_force) + " " + "lbs " + "  " + str(ramp_incline_degrees) + " degree incline   " + str("%.3f" %percentage(total_force, wt)) + " percent of pt total weight")
